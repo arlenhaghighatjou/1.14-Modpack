432dbd70f14a356c488d80fa66316f9234338521
arlenhaghighatjou <349871234.o@gmail.com>
2026-09-05 12:02:49 -0400

    feat: port farmersdelight renderers to laxs glstatemanager

--- src/mods/java/vectorwing/farmersdelight/client/tileentity/renderer/CuttingBoardTileEntityRenderer.java
+++ src/mods/java/vectorwing/farmersdelight/client/tileentity/renderer/CuttingBoardTileEntityRenderer.java
@@ -1,13 +1,10 @@
 package vectorwing.farmersdelight.client.tileentity.renderer;
 
-import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.platform.GlStateManager;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.ItemRenderer;
-import net.minecraft.client.renderer.Vector3f;
 import net.minecraft.client.renderer.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
-import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.item.HoeItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.PickaxeItem;
@@ -17,74 +14,48 @@ import vectorwing.farmersdelight.tile.CuttingBoardTileEntity;
 
 public class CuttingBoardTileEntityRenderer extends TileEntityRenderer<CuttingBoardTileEntity>
 {
-	public CuttingBoardTileEntityRenderer(TileEntityRendererDispatcher rendererDispatcherIn) { super(rendererDispatcherIn); }
-
 	@Override
-	public void render(CuttingBoardTileEntity tileEntityIn, float partialTicks, MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int combinedLightIn, int combinedOverlayIn)
+	public void render(CuttingBoardTileEntity tileEntityIn, double x, double y, double z, float partialTicks, int destroyStage)
 	{
 		Direction direction = tileEntityIn.getBlockState().get(CuttingBoardBlock.FACING).getOpposite();
 		ItemStack itemStack = tileEntityIn.getStoredItem();
 
 		if (!itemStack.isEmpty()) {
-			matrixStackIn.push();
+			ItemRenderer itemRenderer = Minecraft.getInstance().getItemRenderer();
+			boolean blockItem = itemRenderer.getItemModelWithOverrides(itemStack, tileEntityIn.getWorld(), null).isGui3d();
 
-			ItemRenderer itemRenderer = Minecraft.getInstance()
-					.getItemRenderer();
-			boolean blockItem = itemRenderer.getItemModelWithOverrides(itemStack, tileEntityIn.getWorld(), null)
-					.isGui3d();
+			GlStateManager.pushMatrix();
 
 			if (tileEntityIn.getIsItemCarvingBoard()) {
-				renderItemCarved(matrixStackIn, direction, itemStack);
+				renderItemCarved(x, y, z, direction, itemStack);
 			} else if (blockItem) {
-				renderBlock(matrixStackIn, direction);
+				renderBlock(x, y, z, direction);
 			} else {
-				renderItemLayingDown(matrixStackIn, direction);
+				renderItemLayingDown(x, y, z, direction);
 			}
 
-			Minecraft.getInstance().getItemRenderer().renderItem(itemStack, ItemCameraTransforms.TransformType.FIXED, combinedLightIn, combinedOverlayIn, matrixStackIn, bufferIn);
-			matrixStackIn.pop();
+			itemRenderer.renderItem(itemStack, ItemCameraTransforms.TransformType.FIXED);
+			GlStateManager.popMatrix();
 		}
 	}
 
-	public void renderItemLayingDown(MatrixStack matrixStackIn, Direction direction) {
-		// Center item above the cutting board
-		matrixStackIn.translate(0.5D, 0.08D, 0.5D);
-
-		// Rotate item to face the cutting board's front side
-		float f = -direction.getHorizontalAngle();
-		matrixStackIn.rotate(Vector3f.YP.rotationDegrees(f));
-
-		// Rotate item flat on the cutting board. Use X and Y from now on
-		matrixStackIn.rotate(Vector3f.XP.rotationDegrees(90.0F));
-
-		// Resize the item
-		matrixStackIn.scale(0.6F, 0.6F, 0.6F);
+	public void renderItemLayingDown(double x, double y, double z, Direction direction) {
+		GlStateManager.translatef((float)x + 0.5F, (float)y + 0.08F, (float)z + 0.5F);
+		GlStateManager.rotatef(-direction.getHorizontalAngle(), 0.0F, 1.0F, 0.0F);
+		GlStateManager.rotatef(90.0F, 1.0F, 0.0F, 0.0F);
+		GlStateManager.scalef(0.6F, 0.6F, 0.6F);
 	}
 
-	public void renderBlock(MatrixStack matrixStackIn, Direction direction) {
-		// Center block above the cutting board
-		matrixStackIn.translate(0.5D, 0.27D, 0.5D);
-
-		// Rotate block to face the cutting board's front side
-		float f = -direction.getHorizontalAngle();
-		matrixStackIn.rotate(Vector3f.YP.rotationDegrees(f));
-
-		// Resize the block
-		matrixStackIn.scale(0.8F, 0.8F, 0.8F);
+	public void renderBlock(double x, double y, double z, Direction direction) {
+		GlStateManager.translatef((float)x + 0.5F, (float)y + 0.27F, (float)z + 0.5F);
+		GlStateManager.rotatef(-direction.getHorizontalAngle(), 0.0F, 1.0F, 0.0F);
+		GlStateManager.scalef(0.8F, 0.8F, 0.8F);
 	}
 
-	public void renderItemCarved(MatrixStack matrixStackIn, Direction direction, ItemStack itemStack) {
-		// Center item above the cutting board
-		matrixStackIn.translate(0.5D, 0.25D, 0.5D);
-
-		// Rotate item to face the cutting board's front side
-		float f = -direction.getHorizontalAngle();
-		matrixStackIn.rotate(Vector3f.YP.rotationDegrees(f));
-
-		// Rotate item to be carved on the surface, A little less so for hoes and pickaxes.
-		matrixStackIn.rotate(Vector3f.ZP.rotationDegrees(itemStack.getItem() instanceof PickaxeItem || itemStack.getItem() instanceof HoeItem ? 225.0F : 180.0F));
-
-		// Resize the item
-		matrixStackIn.scale(0.6F, 0.6F, 0.6F);
+	public void renderItemCarved(double x, double y, double z, Direction direction, ItemStack itemStack) {
+		GlStateManager.translatef((float)x + 0.5F, (float)y + 0.25F, (float)z + 0.5F);
+		GlStateManager.rotatef(-direction.getHorizontalAngle(), 0.0F, 1.0F, 0.0F);
+		GlStateManager.rotatef(itemStack.getItem() instanceof PickaxeItem || itemStack.getItem() instanceof HoeItem ? 225.0F : 180.0F, 0.0F, 0.0F, 1.0F);
+		GlStateManager.scalef(0.6F, 0.6F, 0.6F);
 	}
 }
--- src/mods/java/vectorwing/farmersdelight/client/tileentity/renderer/StoveTileEntityRenderer.java
+++ src/mods/java/vectorwing/farmersdelight/client/tileentity/renderer/StoveTileEntityRenderer.java
@@ -1,54 +1,36 @@
 package vectorwing.farmersdelight.client.tileentity.renderer;
 
-import net.minecraft.client.renderer.WorldRenderer;
-import vectorwing.farmersdelight.blocks.StoveBlock;
-import vectorwing.farmersdelight.tile.StoveTileEntity;
-import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.platform.GlStateManager;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.IRenderTypeBuffer;
-import net.minecraft.client.renderer.Vector3f;
 import net.minecraft.client.renderer.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
-import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Direction;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.Vec2f;
+import vectorwing.farmersdelight.blocks.StoveBlock;
+import vectorwing.farmersdelight.tile.StoveTileEntity;
 
 public class StoveTileEntityRenderer extends TileEntityRenderer<StoveTileEntity>
 {
-	public StoveTileEntityRenderer(TileEntityRendererDispatcher rendererDispatcher) {
-		super(rendererDispatcher);
-	}
-
-	public void render(StoveTileEntity tileEntityIn, float partialTicks, MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int combinedLightIn, int combinedOverlayIn) {
+	@Override
+	public void render(StoveTileEntity tileEntityIn, double x, double y, double z, float partialTicks, int destroyStage)
+	{
 		Direction direction = tileEntityIn.getBlockState().get(StoveBlock.FACING).getOpposite();
 		NonNullList<ItemStack> nonnulllist = tileEntityIn.getInventory();
 
 		for(int i = 0; i < nonnulllist.size(); ++i) {
 			ItemStack itemstack = nonnulllist.get(i);
 			if (!itemstack.isEmpty()) {
-				matrixStackIn.push();
-
-				// Center item above the stove
-				matrixStackIn.translate(0.5D, 1.02D, 0.5D);
-
-				// Rotate item to face the stove's front side
-				float f = -direction.getHorizontalAngle();
-				matrixStackIn.rotate(Vector3f.YP.rotationDegrees(f));
-
-				// Rotate item flat on the stove. Use X and Y from now on
-				matrixStackIn.rotate(Vector3f.XP.rotationDegrees(90.0F));
-
-				// Neatly align items according to their index
+				GlStateManager.pushMatrix();
+				GlStateManager.translatef((float)x + 0.5F, (float)y + 1.02F, (float)z + 0.5F);
+				GlStateManager.rotatef(-direction.getHorizontalAngle(), 0.0F, 1.0F, 0.0F);
+				GlStateManager.rotatef(90.0F, 1.0F, 0.0F, 0.0F);
 				Vec2f itemOffset = tileEntityIn.getStoveItemOffset(i);
-				matrixStackIn.translate(itemOffset.x, itemOffset.y, 0.0D);
-
-				// Resize the items
-				matrixStackIn.scale(0.375F, 0.375F, 0.375F);
-
-				Minecraft.getInstance().getItemRenderer().renderItem(itemstack, ItemCameraTransforms.TransformType.FIXED, WorldRenderer.getCombinedLight(tileEntityIn.getWorld(), tileEntityIn.getPos().up()), combinedOverlayIn, matrixStackIn, bufferIn);
-				matrixStackIn.pop();
+				GlStateManager.translatef(itemOffset.x, itemOffset.y, 0.0F);
+				GlStateManager.scalef(0.375F, 0.375F, 0.375F);
+				Minecraft.getInstance().getItemRenderer().renderItem(itemstack, ItemCameraTransforms.TransformType.FIXED);
+				GlStateManager.popMatrix();
 			}
 		}
 	}
