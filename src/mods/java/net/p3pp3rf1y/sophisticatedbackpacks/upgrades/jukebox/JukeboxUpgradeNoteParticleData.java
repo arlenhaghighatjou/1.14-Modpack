e080ff994ae1f4f1e9d3a6068717f2c3a0f765fd
arlenhaghighatjou <349871234.o@gmail.com>
2026-09-06 01:40:14 -0400

    feat: port the jukebox particle type to 1.14

--- src/mods/java/net/p3pp3rf1y/sophisticatedbackpacks/upgrades/jukebox/JukeboxUpgradeNoteParticleData.java
+++ src/mods/java/net/p3pp3rf1y/sophisticatedbackpacks/upgrades/jukebox/JukeboxUpgradeNoteParticleData.java
@@ -1,10 +1,10 @@
 package net.p3pp3rf1y.sophisticatedbackpacks.upgrades.jukebox;
 
 import com.mojang.brigadier.StringReader;
-import com.mojang.serialization.Codec;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.particles.IParticleData;
 import net.minecraft.particles.ParticleType;
+import net.minecraft.util.registry.Registry;
 import net.p3pp3rf1y.sophisticatedbackpacks.init.ModParticles;
 
 public class JukeboxUpgradeNoteParticleData extends ParticleType<JukeboxUpgradeNoteParticleData> implements IParticleData {
@@ -18,32 +18,25 @@ public class JukeboxUpgradeNoteParticleData extends ParticleType<JukeboxUpgradeN
 	}
 
 	@Override
-	public void writeToNetwork(PacketBuffer pBuffer) {
+	public void write(PacketBuffer pBuffer) {
 		//noop
 	}
 
 	@Override
-	public String writeToString() {
+	public String getParameters() {
 		//noinspection ConstantConditions
-		return ModParticles.JUKEBOX_NOTE.get().getRegistryName().toString();
+		return Registry.PARTICLE_TYPE.getKey(ModParticles.JUKEBOX_NOTE.get()).toString();
 	}
 
 	public static final IDeserializer<JukeboxUpgradeNoteParticleData> DESERIALIZER = new IDeserializer<JukeboxUpgradeNoteParticleData>() {
 		@Override
-		public JukeboxUpgradeNoteParticleData fromCommand(ParticleType<JukeboxUpgradeNoteParticleData> pParticleType, StringReader pReader) {
+		public JukeboxUpgradeNoteParticleData deserialize(ParticleType<JukeboxUpgradeNoteParticleData> pParticleType, StringReader pReader) {
 			return (JukeboxUpgradeNoteParticleData) pParticleType;
 		}
 
 		@Override
-		public JukeboxUpgradeNoteParticleData fromNetwork(ParticleType<JukeboxUpgradeNoteParticleData> pParticleType, PacketBuffer pBuffer) {
+		public JukeboxUpgradeNoteParticleData read(ParticleType<JukeboxUpgradeNoteParticleData> pParticleType, PacketBuffer pBuffer) {
 			return (JukeboxUpgradeNoteParticleData) pParticleType;
 		}
 	};
-
-	private final Codec<JukeboxUpgradeNoteParticleData> codec = Codec.unit(this::getType);
-
-	@Override
-	public Codec<JukeboxUpgradeNoteParticleData> codec() {
-		return codec;
-	}
 }
